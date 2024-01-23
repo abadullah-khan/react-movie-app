69a5d2b710aa4cec45e5663d91195100dfd60be9
Abadullah Khan <abadullahk73@gmail.com>
2024-01-23 19:44:01 +0530

    Renamed the Movies.jsx file to Medi.jsx file.

---
+++ src/components/movies/Media.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import { getData } from "../../Slices/MoviesSlice";
+import { Card } from "../Card";
+
+export const Movies = () => {
+  const dispatch = useDispatch();
+  const { data, currentPage } = useSelector((state) => state.movies);
+  const { media_type, movies_type } = useParams();
+
+  useEffect(() => {
+    dispatch(getData({ media_type, movies_type, currentPage }));
+  }, [media_type, movies_type]);
+
+  return (
+    <>
+      <div className="moviesContainer">
+        <div className="moviesWrapper">
+          {data.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
