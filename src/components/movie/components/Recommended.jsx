397e60f02fba710662273384652470f1bb5b3b67
Abadullah Khan <abadullahk73@gmail.com>
2024-03-05 23:41:00 +0530

    Renamed the class .header to sectionName and changed its font color.

--- src/components/movie/components/Recommended.jsx
+++ src/components/movie/components/Recommended.jsx
@@ -4,7 +4,7 @@ import { Card } from "../../Card";
 const Recommended = ({ movieDetails }) => {
   return (
     <div className="recommendedWrapper">
-      <div className="header">Recommendations</div>
+      <div className="sectionName">Recommendations</div>
       <div className="movieCardContainer">
         {movieDetails.recommendations.results.map((item) => (
           <Card item={item} />
